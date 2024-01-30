11e8dcf833af7a68ed9bf62bbe766562d09c105c
Derek Ayala <derek_ayala@epam.com>
2024-01-30 11:12:06 -0600

    Initial commit

---
+++ src/test/java/com/example/books/AuthorTest.java
@@ -0,0 +1,20 @@
+package com.example.books;
+
+import com.example.books.model.Author;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AuthorTest {
+
+    @Test
+    public void testAuthorModel() {
+        Author author = new Author("John Doe");
+
+        Assertions.assertEquals(1L, author.getId());
+        Assertions.assertEquals("John Doe", author.getName());
+
+        author.setName("Jane Smith");
+
+        Assertions.assertEquals("Jane Smith", author.getName());
+    }
+}
